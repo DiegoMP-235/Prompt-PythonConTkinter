6cc2b513cc0ff5d165adb53c0cb369fa5b67ccc9
DiegoMP-235 <121039161@upq.edu.mx>
2023-03-14 22:51:10 -0600

    Create main.py
    Se instancia un objeto tipo prompt, solo falta ajustar que se espere a que se cierre la ventana en la misma clase del prompt

---
+++ main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from Prompt import *
+
+def muestraPrompt():
+    myPrompt = Prompt(ventana,"Mi prompt","Ingresa tu valor","Guardar")
+    ventana.wait_window(myPrompt.getVentana())
+    Valor_ingresado = myPrompt.getValue()
+    print("El valor ingresado:"+Valor_ingresado)
+
+
+ventana = Tk()
+ventana.title("Mi ventana principal")
+ventana.geometry("350x350")
+
+myButton = Button(ventana,text="Abrir Prompt",command=muestraPrompt)
+myButton.pack()
+
+ventana.mainloop()
